7436a527da4722a2b5dcf1172046ed5e7e687d2a
Christian Katzorke <c.katzorke@web.de>
2013-12-05 01:04:30 +0100

    added new tests

--- markup-js-express.js
+++ markup-js-express.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var util = require('util');
 var markupjsexpress = (function() {
 	"use strict";
-	var extension = "markupjs";
+	var templateExtension = "markupjs";
 
 	var loadTemplate = function(file, callback) {
 		util.log("Loading template '" + file + "'");
@@ -22,10 +22,11 @@ var markupjsexpress = (function() {
 
 	return {
 		"create": function(dir, extension) {
+			templateExtension = extension || templateExtension;
 			var renderer = function(template, options, callback) {
 				util.log("Rendering '" + template + "' with " + JSON.stringify(options));
 				var viewDirectory = dir || options.settings.views;
-				var viewExtension = '.' + (extension || options.settings['view engine']);
+				var viewExtension = '.' + (templateExtension || options.settings['view engine']);
 				var file = viewDirectory + "/" + template + viewExtension;
 
 				loadTemplate(file, function(err, data) {
--- test/test-markup-js-express.js
+++ test/test-markup-js-express.js
@@ -14,6 +14,18 @@ describe('markupjsExpress', function() {
 		});
 	});
 
+	it('should render a simple template, even without passing the extesion (defaults to markupjs)', function(done) {
+		var renderer = markupjsExpress('test/templates');
+		renderer('simple', {
+			greeting: "Hello, World"
+		}, function(err, result) {
+			should.not.exist(err);
+			should.exist(result);
+			result.should.eql("<h1>Hello, World</h1>");
+			done();
+		});
+	});
+
 	it('should throw an error when template does not exist', function(done) {
 		var renderer = markupjsExpress('test/templates', 'markupjs');
 		renderer('nonexisting', {
